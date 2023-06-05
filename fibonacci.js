d6b5448cafecc702514943bf37e7e9ef0c97d1aa
Jarrett Lyon <64499632+probableactions@users.noreply.github.com>
2023-06-05 15:10:42 -0600

    Solve fibonacci by recursion

--- fibonacci.js
+++ fibonacci.js
@@ -1,12 +1,23 @@
 // Fibonacci sequence by iteration
 const fibs = (n) => {
-	let fibs = [0, 1];
+	let seq = [0, 1];
 
-	for (let i = fibs.length; i < n; i++) {
-		fibs[i] = fibs[i - 1] + fibs[i - 2];
+	for (let i = seq.length; i < n; i++) {
+		seq[i] = seq[i - 1] + seq[i - 2];
 	}
-	return fibs;
+	return seq;
 };
 
 fibs(8);
+// Output : [ 0, 1, 1, 2, 3, 5, 8, 13 ]
+
+const fibsRec = (n, seq = [0, 1]) => {
+	let currentSequence = seq.length;
+	if (currentSequence >= n) return seq;
+	return fibsRec(n, [
+		...seq,
+		seq[currentSequence - 1] + seq[currentSequence - 2],
+	]);
+};
+fibsRec(8);
 // Output : [ 0, 1, 1, 2, 3, 5, 8, 13 ]
